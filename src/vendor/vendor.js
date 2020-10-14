cc91253ff3fcef0621e18e0f41be3273225b9b93
Заур <zaurmag411@gmail.com>
2020-10-14 17:53:55 +0300

    Завершил верстку блока захвата телефона

--- src/vendor/vendor.js
+++ src/vendor/vendor.js
@@ -1,8 +1,8 @@
 // import './go-top/go-top';
 
 // Jquery Input mask
-// import './inputmask-bundle/jquery.inputmask.bundle.min';
-// import './inputmask-bundle/init';
+import './inputmask-bundle/jquery.inputmask.bundle.min';
+import './inputmask-bundle/init';
 
 // Jquery Form Styler
 // import './jquery-formstyler/jquery.formstyler.min';
