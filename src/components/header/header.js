adf4ee521ac62f54833799e4c3262f06b9e2c232
Заур <zaurmag411@gmail.com>
2020-10-22 17:57:55 +0300

    Плавный скролл до верха контейнера при переходе к следующему вопросу.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,9 @@
+import {animateScroll} from '../../vendor/animate-scroll';
+
 let btn = $('.btn-flare--js');
 let main = $('#main');
 
-btn.click(() => {
-	$('html, body').animate({
-		scrollTop: `${main.offset().top}px`,
-	}, {
-		duration: 300,
-	});
-
-	return false;
+btn.click((e) => {
+	e.preventDefault();
+	animateScroll(main, 300);
 });
